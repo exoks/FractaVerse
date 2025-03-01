f9f5723821d1903c4fe7d4b5e69c2faba8a1ce6d
Oussama Ezzaou <oussamaezzaou@gmail.com>
2025-03-01 15:14:13 +0000

    fract-ol: ft_printf:latest

--- ft_printf/libft/src/ft_strlen.c
+++ ft_printf/libft/src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: oezzaou <oezzaou@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 21:16:13 by oezzaou           #+#    #+#             */
-/*   Updated: 2022/10/02 18:00:17 by oezzaou          ###   ########.fr       */
+/*   Updated: 2025/03/01 15:06:58 by oezzaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,7 +14,18 @@
 //====<[ ft_strlen: ]>==========================================================
 size_t	ft_strlen(const char *s)
 {
-	if (*s)
-		return (1 + ft_strlen(++s));
-	return (0);
+	int	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
+// size_t	ft_strlen(const char *s)
+// {
+// 	if (s == NULL)
+// 		return (-1);
+// 	if (*s)
+// 		return (1 + ft_strlen(++s));
+// 	return (0);
+// }
